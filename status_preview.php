67f721747652a4cbc99ee160aad187b3884fba0e
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-07-22 00:03:56 +0600

    up to date

--- status_preview.php
+++ status_preview.php
@@ -2,8 +2,14 @@
     // to access the track_parcel() function
     include('functions.php');
 
+    // fetch your pracel tracking code from the database for the specific order no/invoice-no
+    // -- Run SQL query to get the `tracking_code`
+
+    // Example tracking code, replace with actual code from your database
+    $order_tarcking_code = "123456789"; 
+
     // Parcel Status Preview
-    if (isset($order['tracking_code'])) {
+    if (isset($order_tarcking_code)) {
         $status = track_parcel($order['tracking_code']);
     } else {
         $status = "Not Added";
